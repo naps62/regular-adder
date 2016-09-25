cae4a1186de483a3b513a0ae3f672a64f3654257
Miguel Palhas <mpalhas@gmail.com>
2016-09-26 00:39:21 +0100

    code review

--- regular_adder_spec.rb
+++ regular_adder_spec.rb
@@ -10,6 +10,29 @@ RSpec.describe RegularAdder do
   it "works for 12337 + 173" do
     adder = RegularAdder.new("+ 12337 173")
 
-    expect(adder.parse).to eq "12510"
+    expect(adder.parse).to eq "12532"
+  end
+
+  it "works for 1 + 0" do
+    adder = RegularAdder.new("+ 1 0")
+
+    expect(adder.parse).to eq "1"
+  end
+
+  it "works for 10 + 10" do
+    adder = RegularAdder.new("+ 10 10")
+
+    expect(adder.parse).to eq "20"
+  end
+
+  it "works for 0 + 0" do
+    adder = RegularAdder.new("+ 0 0")
+
+    expect(adder.parse).to eq "0"
+  end
+  it "works for 0 + 1" do
+    adder = RegularAdder.new("+ 0 1")
+
+    expect(adder.parse).to eq "1"
   end
 end
